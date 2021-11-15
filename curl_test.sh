91bcb295c752c908443eb720707b355b3f535ace
Keren Zhou <Keren.Zhou@tufts.edu>
2021-11-14 19:58:09 -0500

    curl_test: add CONNECT test

--- curl_test.sh
+++ curl_test.sh
@@ -134,6 +134,39 @@ echo '- compare with no response header'
 diff ${name}-response-no-header.txt ${name}-response-proxy-no-header.txt
 echo
 
+# Test CONNECT method.
+name='connect'
+url='https://www.cplusplus.com/reference/cstring/strcat/'
+echo "CONNECT ${url}"
+# Direct access with response header.
+curl "${url}" \
+--insecure --verbose --include \
+--output ${name}-response.txt \
+--stderr ${name}-verbose.txt
+# Direct access with no response header.
+curl "${url}" \
+--insecure --verbose \
+--output ${name}-response-no-header.txt \
+--stderr ${name}-verbose-no-header.txt
+# Proxy access with response header.
+curl "${url}" \
+--insecure --verbose --include \
+--proxy ${proxy} \
+--output ${name}-response-proxy.txt \
+--stderr ${name}-verbose-proxy.txt
+# Proxy access with no response header.
+curl "${url}" \
+--insecure --verbose \
+--proxy ${proxy} \
+--output ${name}-response-proxy-no-header.txt \
+--stderr ${name}-verbose-proxy-no-header.txt
+# Compare direct and proxy access.
+echo '- compare with response header'
+diff ${name}-response.txt ${name}-response-proxy.txt
+echo '- compare with no response header'
+diff ${name}-response-no-header.txt ${name}-response-proxy-no-header.txt
+echo
+
 # # Test with SCP wiki, which uses chunked transfer encoding.
 # name='scp'
 # url='http://scp-wiki-cn.wikidot.com/'
